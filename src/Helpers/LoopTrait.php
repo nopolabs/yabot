9602961afbce4fbda0f44e802ea194600a396504
drevel <drevel@opensky.com>
2017-03-02 18:10:26 -0800

    breakout plugin config

--- src/Helpers/LoopTrait.php
+++ src/Helpers/LoopTrait.php
@@ -21,18 +21,25 @@ trait LoopTrait
         return $this->loop;
     }
 
-    protected function addTimer($interval, callable $callback)
+    protected function addTimer($interval, $function)
     {
-        $this->getLoop()->addTimer($interval, $callback);
+        $this->getLoop()->addTimer($interval, $this->callable($function));
     }
 
-    protected function addPeriodicTimer($interval, callable $callback)
+    protected function addPeriodicTimer($interval, $function)
     {
-        $this->getLoop()->addPeriodicTimer($interval, $callback);
+        $this->getLoop()->addPeriodicTimer($interval, $this->callable($function));
     }
 
     protected function cancelTimer(TimerInterface $timer)
     {
         $this->getLoop()->cancelTimer($timer);
+    }
+
+    private function callable($function) : callable
+    {
+        return function () use ($function) {
+            call_user_func($function);
+        };
     }
 }
--- yabot.php
+++ yabot.php
@@ -1,7 +1,10 @@
 <?php
+use Nopolabs\Yabot\Bot\PluginInterface;
+use Nopolabs\Yabot\Yabot;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 require __DIR__.'/vendor/autoload.php';
 
@@ -9,6 +12,17 @@ $container = new ContainerBuilder();
 $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/config'));
 $loader->load('services.xml');
 
+$plugins = new YamlFileLoader($container, new FileLocator(__DIR__.'/config'));
+$plugins->load('plugins.yml');
+
+/** @var Yabot $yabot */
 $yabot = $container->get('yabot');
+
+$pluginIds = $container->findTaggedServiceIds('yabot.plugin');
+foreach ($pluginIds as $pluginId => $value) {
+    /** @var PluginInterface $plugin */
+    $plugin = $container->get($pluginId);
+    $yabot->addPlugin($plugin);
+}
 
 $yabot->run();
