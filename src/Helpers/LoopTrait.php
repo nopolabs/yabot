fc135b8ad74e0fcc9c4f43a4f0667623806aefe6
drevel <drevel@opensky.com>
2017-06-04 13:55:10 -0700

    handle exceptions in timer callbacks

--- src/Helpers/LoopTrait.php
+++ src/Helpers/LoopTrait.php
@@ -2,14 +2,16 @@
 
 namespace Nopolabs\Yabot\Helpers;
 
-
 use React\EventLoop\LoopInterface;
 use React\EventLoop\Timer\TimerInterface;
+use Throwable;
 
 trait LoopTrait
 {
     /** @var LoopInterface */
     private $loop;
+
+    abstract protected function warning($message, array $context = array());
 
     protected function setLoop(LoopInterface $loop)
     {
@@ -39,7 +41,14 @@ trait LoopTrait
     private function callable($function) : callable
     {
         return function() use ($function) {
-            call_user_func($function);
+            try {
+                call_user_func($function);
+            } catch (Throwable $throwable) {
+                $errmsg = "Unhandled Exception in Timer callback\n"
+                    .$throwable->getMessage()."\n"
+                    .$throwable->getTraceAsString();
+                $this->warning($errmsg);
+            }
         };
     }
 }
--- tests/Helpers/LoopTraitTest.php
+++ tests/Helpers/LoopTraitTest.php
@@ -2,19 +2,24 @@
 namespace Nopolabs\Yabot\Tests\Helpers;
 
 use Closure;
+use Exception;
 use Nopolabs\Test\MockWithExpectationsTrait;
+use Nopolabs\Yabot\Helpers\LogTrait;
 use Nopolabs\Yabot\Helpers\LoopTrait;
 use PHPUnit\Framework\TestCase;
+use Psr\Log\LoggerInterface;
 use React\EventLoop\LoopInterface;
 use React\EventLoop\Timer\TimerInterface;
 
 class LoopTraitTest extends TestCase
 {
     use MockWithExpectationsTrait;
+    use LogTrait;
     use LoopTrait;
 
     protected function setUp()
     {
+        $this->setLog($this->createMock(LoggerInterface::class));
         $this->setLoop($this->createMock(LoopInterface::class));
     }
 
@@ -45,5 +50,39 @@ class LoopTraitTest extends TestCase
         ]);
 
         $this->cancelTimer($timer);
+    }
+
+    public function testCallable()
+    {
+        $called = false;
+
+        $function = function() use (&$called) {
+            $called = true;
+            return;
+        };
+
+        $callable = $this->callable($function);
+
+        $callable();
+
+        $this->assertTrue($called);
+    }
+
+    public function testCallableHandlesException()
+    {
+        $this->setAtExpectations($this->log, [
+            ['warning', ['params' => [$this->callback(function($errmsg) {
+                $this->assertRegExp("/^Unhandled Exception in Timer callback\ntesting\n#0/m", $errmsg);
+                return true;
+            })]]],
+        ]);
+
+        $function = function() {
+            throw new Exception('testing');
+        };
+
+        $callable = $this->callable($function);
+
+        $callable();
     }
 }
